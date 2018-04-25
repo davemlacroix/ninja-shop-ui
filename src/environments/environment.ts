952673ecc7eca57defe03537ff442e55e74baa0f
ElAndyG <andyg8180@gmail.com>
2018-04-24 20:01:23 -0400

    changing to https for stackblitz

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:5000/api'
-   apiUrl: 'http://ninja-shop-api.azurewebsites.net/api'
+  apiUrl: 'https://ninja-shop-api.azurewebsites.net/api'
 };
